815bbaca61464225dd339200849e3218aaf701a5
rohansingh2002 <121345451+rohansingh2002@users.noreply.github.com>
2023-07-31 23:53:15 +0530

    Time: 49 ms (99.57%), Space: 22.3 MB (39.54%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,32 +1,41 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        int di[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
-        int dj[8] = {0, 1, 1, 1, 0, -1, -1, -1};
-        int n = grid.size();
-        if (grid[0][0] || grid[n-1][n-1]) return -1;
-        queue<pair<int, int>> q;
-        int level = 0;
-        q.push({0, 0});
-        grid[0][0] = 1;
-        while (!q.empty()) {
-            level++;
-            int sz = q.size();
-            while (sz--) {
-                int i = q.front().first, j = q.front().second;
-                q.pop();
-                if (i == n-1 && j == n-1)
-                    return level;
-                for (int k = 0; k < 8; k++) {
-                    int ni = i + di[k];
-                    int nj = j + dj[k];
-                    if (ni >= 0 && ni < n && nj >= 0 && nj < n && grid[ni][nj] == 0) {
-                        grid[ni][nj] = 1;
-                        q.push({ni, nj});
+    
+       int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int>>adj(n,vector<int>(m,1e9));
+        queue<pair<int,pair<int,int>>>q;
+        if(grid[0][0]==1){
+            return -1;
+        }
+        if(n==1 && m==1 && grid[0][0]==0){
+            return 1;
+        }
+        q.push({1,{0,0}});
+        int ans=0;
+        while(!q.empty()){
+            auto it=q.front();
+            q.pop();
+            int step=it.first;
+            int row=it.second.first;
+            int col=it.second.second;
+            for(int i=-1;i<=1;i++){
+                for(int j=-1;j<=1;j++){
+                    int newr=row+i;
+                    int newc=col+j;
+                    if(newr>=0 && newr<n && newc>=0 && newc<m && grid[newr][newc]!=1 && step+1<adj[newr][newc] ){
+                    adj[newr][newc]=step+1;
+                    if(newr==n-1 && newc==m-1){
+                        return step+1;
+                    }
+                    q.push({step+1,{newr,newc}});
                     }
                 }
             }
         }
         return -1;
+        
+        
     }
 };
