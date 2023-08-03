eaa5ed4861859fb0214afdbf9a9b9e30ac8df73b
rohansingh2002 <121345451+rohansingh2002@users.noreply.github.com>
2023-08-04 02:46:40 +0530

    Time: 3 ms (42.30%), Space: 6.3 MB (58.70%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.cpp
@@ -3,12 +3,18 @@ public:
     int minAddToMakeValid(string s) {
         int status = 0,count = 0;
         for(int i = 0; i < s.size(); i++){
-            if(s[i] == '(') status++;
-            else status--;
-            if(status < 0) {count++;
-            status++;}
+            if(s[i] == '('){
+                status++;
+            }
+            else {
+                if(status > 0){
+                  status--;
+                }
+                else{
+                  count++;
+                }
+            }
         }
-        if(status > 0) return count+status;
-        return count;
+        return count+status;   
     }
 };
