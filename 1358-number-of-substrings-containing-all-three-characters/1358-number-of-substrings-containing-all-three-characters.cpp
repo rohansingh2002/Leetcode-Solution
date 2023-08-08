54d412f5a96ba9314d32a36bdd47cc77868eba0d
rohansingh2002 <121345451+rohansingh2002@users.noreply.github.com>
2023-08-08 12:54:38 +0530

    Time: 60 ms (57.44%), Space: 8.5 MB (39.04%) - LeetHub

---
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+         int left = 0 , right = 0 , end = s.size() - 1;
+        unordered_map<char,int> map;
+        
+        int count = 0;
+        
+        while(right != s.size())
+        {
+            map[s[right]] += 1; // adding new element to window
+            
+            while(map['a']>0 and map['b']>0 and map['c']>0) // while(window is good)
+            {
+                count += 1 + (end - right);
+                
+				// removing first element[means shrinking window from front to form new window]
+                map[s[left]] -= 1; 
+                left++;
+            }
+            right++;
+        }
+        return count; 
+    }
+};
