8b2c95815e6eddd6368c9b61bbc2957079f43635
rohansingh2002 <121345451+rohansingh2002@users.noreply.github.com>
2023-08-18 22:34:55 +0530

    Time: 15 ms (94.21%), Space: 13.7 MB (42.42%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,45 +1,37 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-         vector<pair<int, int>> adj[n];
-        for (auto it : flights)
-        {
-            adj[it[0]].push_back({it[1], it[2]});
+        vector<pair<int,int>>adj[n];
+        for(auto it : flights){
+            adj[it[0]].push_back({it[1],it[2]});
         }
-
-        queue<pair<pair<int, int>,int>> q;
         
-        q.push({{src, 0},0});
-
-       
-        vector<int> dist(n, 1e9);
+        priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<pair<int,pair<int,int>>>>pq;
+        vector<int>dist(n,INT_MAX);
+        
+        pq.push({0,{src,0}});
         dist[src] = 0;
-
-        while (!q.empty())
-        {
-            auto it = q.front();
-            q.pop();
-            int stops = it.second;
-            int node = it.first.first;
-            int cost = it.first.second;
-
-        
-            if (stops > k)
-                continue;
-            for (auto iter : adj[node])
-            {
-                int adjNode = iter.first;
-                int edW = iter.second;
-
-                if (cost + edW < dist[adjNode] && stops <= k)
-                {
-                    dist[adjNode] = cost + edW;
-                    q.push({{adjNode, cost + edW},stops + 1});
+        while(!pq.empty()){
+            int stops = pq.top().first;
+            int node = pq.top().second.first;
+            int dis = pq.top().second.second;
+            pq.pop();
+            
+            if(stops > k) continue;
+            for(auto it : adj[node]){
+                int adjnode = it.first;
+                int adjw = it.second;
+                
+                if(dis + adjw < dist[adjnode] && stops <= k){
+                    dist[adjnode] = dis + adjw;
+                    pq.push({stops+1,{adjnode,dis+adjw}});
+                    
+                    
                 }
             }
+            
         }
-        
-        if (dist[dst] == 1e9)
+       if (dist[dst] == INT_MAX)
             return -1;
         return dist[dst];
     }
