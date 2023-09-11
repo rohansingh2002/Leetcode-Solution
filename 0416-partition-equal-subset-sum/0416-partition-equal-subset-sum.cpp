39158e322ad83eb5ce469ea5c41572719339db96
rohansingh2002 <121345451+rohansingh2002@users.noreply.github.com>
2023-09-11 11:16:55 +0530

    Time: 319 ms (36.16%), Space: 13.1 MB (68.97%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    bool partition(int ind, vector<int>&nums, int target, vector<vector<int>>&dp){
-        if(target == 0) return true;
-        if(ind == 0) return (target == nums[0]);
+//     bool partition(int ind, vector<int>&nums, int target, vector<vector<int>>&dp){
+//         if(target == 0) return true;
+//         if(ind == 0) return (target == nums[0]);
         
-        if(dp[ind][target] != -1)  return dp[ind][target];
+//         if(dp[ind][target] != -1)  return dp[ind][target];
         
-        int not_take = partition(ind-1,nums,target,dp);
+//         int not_take = partition(ind-1,nums,target,dp);
         
-        int take = false;
+//         int take = false;
         
-        if(nums[ind] <= target){
-            take = partition(ind-1,nums,target-nums[ind],dp);
-        }
-        return dp[ind][target] = not_take || take;
-    }
+//         if(nums[ind] <= target){
+//             take = partition(ind-1,nums,target-nums[ind],dp);
+//         }
+//         return dp[ind][target] = not_take || take;
+//     }
     bool canPartition(vector<int>& nums){
         int total_sum = 0;
         for(int i = 0; i < nums.size(); i++){
@@ -25,9 +25,30 @@ public:
         }
         
         int n = nums.size();
+         
+        int k = total_sum/2;
         
-        vector<vector<int>>dp(n,vector<int>((total_sum/2)+1,-1));
+        vector<vector<bool>>dp(n,vector<bool>(k+1,false));
         
-        return partition(n-1,nums,total_sum/2,dp);
+        for(int i = 0; i < n; i++){
+            dp[i][0] = true;
+        }
+        
+        if (nums[0] <= k){
+           dp[0][nums[0]] = true;
+        }
+        
+        for(int ind = 1 ; ind < n ; ind++){
+            for(int target = 1; target <= k ; target++){
+                bool not_take = dp[ind-1][target];        
+                bool take = false;
+                if(nums[ind] <= target){
+                   take = dp[ind-1][target-nums[ind]];
+                }
+                 dp[ind][target] = not_take || take;
+            }
+           
+        }
+        return dp[n-1][k];
     }
 };
