427373fea88a6ab58e644428e28a909476a90a86
rohansingh2002 <121345451+rohansingh2002@users.noreply.github.com>
2023-09-11 10:58:29 +0530

    Time: 242 ms (56.98%), Space: 94.9 MB (46.46%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,49 +1,33 @@
 class Solution {
 public:
-    // bool Partition(int ind, int target, vector<int>nums, vector<vector<bool>>&dp){
-    //     if(ind == 0) return (target == nums[0]);
-    //     if(target == 0) return true;
-    //     if(dp[ind][target] != false)  return dp[ind][target];
-    //     bool not_take = Partition(ind-1, target, nums, dp);
-    //     bool take = false;
-    //     if(nums[ind] < target) {
-    //         take = Partition(ind-1, target - nums[ind], nums, dp);
-    //     }
-    //     return dp[ind][target] = take || not_take;
-    // }
-    bool canPartition(vector<int>& nums) {
-        int n = nums.size();
+    bool partition(int ind, vector<int>&nums, int target, vector<vector<int>>&dp){
+        if(target == 0) return true;
+        if(ind == 0) return (target == nums[0]);
+        
+        if(dp[ind][target] != -1)  return dp[ind][target];
+        
+        int not_take = partition(ind-1,nums,target,dp);
+        
+        int take = false;
+        
+        if(nums[ind] <= target){
+            take = partition(ind-1,nums,target-nums[ind],dp);
+        }
+        return dp[ind][target] = not_take || take;
+    }
+    bool canPartition(vector<int>& nums){
         int total_sum = 0;
-       
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < nums.size(); i++){
             total_sum += nums[i];
         }
         if(total_sum % 2 != 0){
             return false;
         }
-        int k = total_sum/2;
-        vector<vector<bool>> dp(n,vector<bool>(k+1,false));
-    
-        for(int i=0; i<n; i++){
-            dp[i][0] = true;
-        }
         
-        if(nums[0]<=k)
-            dp[0][nums[0]] = true;
+        int n = nums.size();
         
-        for(int ind = 1; ind<n; ind++){
-            for(int target= 1; target<=k; target++){
-                
-                bool notTaken = dp[ind-1][target];
+        vector<vector<int>>dp(n,vector<int>((total_sum/2)+1,-1));
         
-                bool taken = false;
-                    if(nums[ind]<=target)
-                        taken = dp[ind-1][target-nums[ind]];
-            
-                dp[ind][target]= notTaken||taken;
-            }
-        }
-        
-        return dp[n-1][k];
+        return partition(n-1,nums,total_sum/2,dp);
     }
 };
