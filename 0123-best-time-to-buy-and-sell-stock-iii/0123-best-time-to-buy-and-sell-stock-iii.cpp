f292867eda819be44c5f0df98a60c66a1a4b322b
rohansingh2002 <121345451+rohansingh2002@users.noreply.github.com>
2023-09-14 17:08:19 +0530

    Time: 568 ms (32.38%), Space: 218.2 MB (9.84%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>>(2,vector<int>(3,0)));
-        int profit = 0 ;   
-         for(int ind= n-1; ind>=0; ind--){
-              for(int buy=0; buy<=1; buy++){
-                  for(int cap = 1; cap <= 2; cap++){
-                  if(buy==0){// We can buy the stock
-                    profit = max(0+dp[ind+1][0][cap], -prices[ind] + dp[ind+1][1][cap]);
-                    }
-    
-                   if(buy==1){// We can sell the stock
-                       profit = max(0+dp[ind+1][1][cap], prices[ind] + dp[ind+1][0][cap-1]);
-                    }
-            
-                dp[ind][buy][cap]  = profit;
-           }
+    int maxGain(vector<int>&prices, int ind, int buy, int cap,int n,  vector<vector<vector<int>>>&dp){
+        if(ind == n || cap == 0){
+            return 0;
         }
+        if(dp[ind][buy][cap] != -1)  return dp[ind][buy][cap];
+        
+        int profit;
+        if(buy == 0){
+            int not_taken = 0 + maxGain(prices,ind+1,0,cap,n,dp);
+            int taken = -prices[ind] + maxGain(prices,ind + 1, 1, cap,n,dp);
+            profit = max(not_taken,taken);
+        }
+        if(buy == 1){
+            int not_taken = 0 + maxGain(prices,ind+1,1,cap,n,dp);
+            int taken = prices[ind] + maxGain(prices,ind + 1, 0, cap-1,n,dp);
+            profit = max(not_taken,taken);
+        }
+        return dp[ind][buy][cap] = profit;
     }
-            
-    return dp[0][0][2];
+    int maxProfit(vector<int>& prices) {
+        int buy = 0;
+        int ind = 0;
+        int cap = 2;
+        int n = prices.size();
+        vector<vector<vector<int>>>dp(n,vector<vector<int>>(2,vector<int>(3,-1)));
+        return maxGain(prices, ind, buy, cap, n, dp);
     }
 };
